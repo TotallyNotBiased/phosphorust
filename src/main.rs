26f9197831edd4e620a895b415f40bb3b08422ee
TotallyNotBiased <highfunctioningpsychopath@gmail.com>
2026-01-19 23:36:38 +1100

    winit hello world

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,38 @@
+use std::error::Error;
+use winit::{application::ApplicationHandler, event::WindowEvent, event_loop::{self, ActiveEventLoop, EventLoop}, window::{Window, WindowId}};
+
+struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        self.window = Some(event_loop.create_window(Window::default_attributes()).unwrap());
+    }
+    
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("The close button was pressed; stopping.");
+                event_loop.exit();
+            },
+            WindowEvent::RedrawRequested => {
+                self.window.as_ref().unwrap().request_redraw();
+            }
+            _ => (),
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new().unwrap();
+
+    event_loop.set_control_flow(event_loop::ControlFlow::Poll);
+
+    let window_attributes = Window::default_attributes().with_title("phosphorust");
+
+    let mut app = App { window: None };
+    event_loop.run_app(&mut app)?;
 
     Ok(())
 }
