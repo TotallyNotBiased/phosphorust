704e92363f2abaca1094f5a53771cddc08aa14cc
TotallyNotBiased <highfunctioningpsychopath@gmail.com>
2026-02-08 14:57:16 +1100

    line x-swapping

--- rasterizer/src/paint.rs
+++ rasterizer/src/paint.rs
@@ -2,6 +2,12 @@ use cg_common::math::{Point2D};
 use cg_common::canvas::Canvas;
 
 pub fn draw_line(point_a: Point2D, point_b: Point2D, color: u32, canvas: &mut Canvas) {
+    let mut p0 = point_a;
+    let mut p1 = point_b;
+    if p0.x > p1.x {
+        std::mem::swap(&mut p0, &mut p1);
+    }
+
     let a = (point_a.y - point_b.y) / (point_a.x - point_b.x);
     let b = point_a.y - a * point_a.x;
     for x in (point_a.x as i32)..(point_b.x as i32) {
